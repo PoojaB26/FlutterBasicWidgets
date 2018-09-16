f117509a4ef15a7298fab4d41ab97305a5adffb5
PoojaB26 <pbhaumik26@gmail.com>
2018-09-16 18:36:21 +0530

    Added examples to Container

--- lib/container_widget.dart
+++ lib/container_widget.dart
@@ -9,17 +9,15 @@ class ContainerWidget extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         margin: EdgeInsets.all(25.0),
-        decoration: new ShapeDecoration(
-          color: Colors.white,
-          shadows: <BoxShadow>[
-            BoxShadow(color: Colors.black, blurRadius: 15.0)
-          ],
-          shape: new Border.all(
+        decoration: new BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: new BorderRadius.circular(55.0),
+          border: new Border.all(
+            width: 5.0,
             color: Colors.red,
-            width: 8.0,
           ),
         ),
-        child: Center(child: const Text('RGB', textAlign: TextAlign.center)),
+        child: Center(child: const Text('Hello Flutter', textAlign: TextAlign.center)),
 
       ),
     );
@@ -57,7 +55,27 @@ class ContainerWidget extends StatelessWidget {
             width: 8.0,
           ),
         ),
-        child: Center(child: const Text('RGB', textAlign: TextAlign.center)),
+        child: Center(child: const Text('Hello Flutter', textAlign: TextAlign.center)),
+      ),
+* */
+
+/*
+*  TODO Rounded Container
+*
+*  Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.all(25.0),
+        decoration: new BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: new BorderRadius.circular(55.0),
+          border: new Border.all(
+            width: 5.0,
+            color: Colors.red,
+          ),
+        ),
+        child: Center(child: const Text('Hello Flutter', textAlign: TextAlign.center)),
+
       ),
 * */
 
