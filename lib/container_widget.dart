9b8b93296df87750d102aaf48d16fa663becdf99
PoojaB26 <pbhaumik26@gmail.com>
2018-09-16 23:49:01 +0530

    Added screenshots

--- lib/container_widget.dart
+++ lib/container_widget.dart
@@ -9,16 +9,7 @@ class ContainerWidget extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         margin: EdgeInsets.all(25.0),
-        decoration: new BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: new BorderRadius.circular(55.0),
-          border: new Border.all(
-            width: 5.0,
-            color: Colors.red,
-          ),
-        ),
-        child: Center(child: const Text('Hello Flutter', textAlign: TextAlign.center)),
-
+        decoration: FlutterLogoDecoration(),
       ),
     );
   }
