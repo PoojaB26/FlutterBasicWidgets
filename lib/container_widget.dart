14834b961813c89bc990735629646e8884155607
PoojaB26 <pbhaumik26@gmail.com>
2019-01-09 15:55:18 +0530

    Added More examples for Container

--- lib/container_widget.dart
+++ lib/container_widget.dart
@@ -4,8 +4,22 @@ class ContainerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(),
-      body: Container(),
+      body: Center(
+        child: Container(
+          height: 200.0,
+          width: 200.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
+            ),
+              color: Colors.green,
+          ),
+          child: Text('Hello'),
+        ),
+      ),
     );
   }
 }
@@ -93,5 +107,100 @@ class ContainerWidget extends StatelessWidget {
           alignment: Alignment.topCenter,
           child: Image.network('https://picsum.photos/500/400'),
         ),
+*
+* */
+
+/*
+* TODO Container with List of BoxShadow
+*
+* Container(
+          height: 100.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+              BoxShadow(color: Colors.red, blurRadius: 12.0 ),
+              BoxShadow(color: Colors.green, blurRadius: 40.0)
+              ]
+          ),
+        )
+*
+*
+*
+* */
+
+
+/*
+* TODO Container with Image and Rounded Border
+*
+* Container(
+          height: 200.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+              color: Colors.white,
+              image: DecorationImage(fit: BoxFit.cover,
+                  image: NetworkImage('https://picsum.photos/200/300'))
+          ),
+        )
+*
+*
+* */
+
+
+/*
+* TODO Circular Container
+*
+* Container(
+          height: 200.0,
+          width: 200.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(200.0),
+              color: Colors.green,
+          ),
+          child: Text('Hello'),
+        )
+*
+*
+* */
+
+
+/*
+* TODO Container with Horizontal Radius of Left Radius and Right Radius
+*
+* Container(
+          height: 200.0,
+          width: 200.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(20.0),
+              right: Radius.circular(80.0)
+            ),
+              color: Colors.green,
+          ),
+          child: Text('Hello'),
+        )
+*
+* */
+
+
+/*
+* TODO Container with Vertical Radius of Top Radius and Bottom Radius
+*
+* Container(
+          height: 200.0,
+          width: 200.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20.0),
+              bottom: Radius.circular(80.0)
+            ),
+              color: Colors.green,
+          ),
+          child: Text('Hello'),
+        )
 *
 * */
