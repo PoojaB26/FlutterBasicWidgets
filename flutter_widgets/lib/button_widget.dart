1075ffe7368bd3455787ea04087a761933c2b878
Élio Muculo <emuculo25@gmail.com>
2022-12-10 01:25:38 +0200

    fix : updating readme replacing deprecated APi's

--- flutter_widgets/lib/button_widget.dart
+++ flutter_widgets/lib/button_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ButtonWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
